68b5713d038b72a4679c7818e64a809ea81f3562
parkhr <vlhong94@gmail.com>
2019-10-21 11:03:45 +0900

    [+] 팀불러오기, 기본 intent test

--- front/app/src/main/java/com/example/findmatch/ResultActivity.kt
+++ front/app/src/main/java/com/example/findmatch/ResultActivity.kt
@@ -2,11 +2,17 @@ package com.example.findmatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+
+        // 보낸 데이터 받기
+        val data = intent.getStringExtra("hi")
+        Toast.makeText(this, "$data", Toast.LENGTH_SHORT).show()
     }
 }
--- springserver/src/main/java/com/ssafy/springserver/Repository/TeamRepository.java
+++ springserver/src/main/java/com/ssafy/springserver/Repository/TeamRepository.java
@@ -1,4 +1,11 @@
 package com.ssafy.springserver.Repository;
 
-public class TeamRepository {
+import com.ssafy.springserver.Entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface TeamRepository extends JpaRepository<Team, String> {
+    @Query("SELECT team FROM Team team WHERE team_name = :teamName")
+    Team findByName(@Param("teamName") String teamName);
 }
