5d98ce624ee92a47c0d32d0065caea60ec49ccd4
김찬송 <rlacksthd1@naver.com>
2019-10-25 17:42:08 +0900

    S1P3151004-43 팀원 추가 기능 및 back-end연걸

--- front/app/src/main/java/com/example/findmatch/MakeTeamActivity.kt
+++ front/app/src/main/java/com/example/findmatch/MakeTeamActivity.kt
@@ -3,32 +3,50 @@ package com.example.findmatch
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.get
+import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_make_team.*
+import kotlinx.android.synthetic.main.activity_team_manage.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.jetbrains.anko.editText
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import android.text.TextWatcher as TextWatcher
 
 class MakeTeamActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_team)
+        var checkDup = false
 
+        var addMember = false
+        var teamMember = HashSet<String>()
         DupBtn.setOnClickListener {
-            checkDuplication()
+            checkDup = checkDuplication()
+        }
+        TeamNameTxt.addTextChangedListener(){
+            checkDup = false
+            dupCheckText.setText("팀 이름이 변경되었습니다")
         }
         addMemberBtn.setOnClickListener{
-            addTeamMember()
+            addTeamMember(teamMember)
+
+        }
+        TeamMakeCommit.setOnClickListener {
+            
         }
     }
-    private fun checkDuplication(){
+
+    private fun checkDuplication():Boolean{
         val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:8080")
             .addConverterFactory(GsonConverterFactory.create())
             .client(createOkHttpClient())
@@ -37,28 +55,27 @@ class MakeTeamActivity : AppCompatActivity() {
         val teamName = TeamNameTxt.text.toString()
 
         var service = retrofit.create(TeamService::class.java)
-
+        var check = false
         val call: Call<TeamDto> = service.duplicationTeamName(teamName)
         call.enqueue(object : Callback<TeamDto>{
             override fun onFailure(call: Call<TeamDto>, t: Throwable) {
                 Toast.makeText(applicationContext,"실패",Toast.LENGTH_SHORT).show()
             }
-
             override fun onResponse(call: Call<TeamDto>, response: Response<TeamDto>) {
                 if(response.body()!=null){
-
                     dupCheckText.setText("등록된 팀 이름이 있습니다. 다시 설정해주세요.")
                     dupCheckText.setTextColor(Color.RED)
-
+                    check = false
                 }else{
                     dupCheckText.setText("사용 가능한 팀 명입니다.")
                     dupCheckText.setTextColor(Color.BLUE)
-
+                    check = true
                 }
             }
         })
+        return check
     }
-    private fun addTeamMember(){
+    private fun addTeamMember(teamMember : HashSet<String>){
         val retrofit = Retrofit.Builder().baseUrl("http://10.0.2.2:8080")
             .addConverterFactory(GsonConverterFactory.create())
             .client(createOkHttpClient())
@@ -75,9 +92,16 @@ class MakeTeamActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 if(response.body()!=null){
-                    EmailCheckText.setText("추가 성공!")
-                    EmailCheckText.setTextColor(Color.BLUE)
-                    addText(MemberEmail)
+                    if(teamMember.contains(MemberEmail)){
+                        EmailCheckText.setText("이미 등록된 맴버입니다.")
+                        EmailCheckText.setTextColor(Color.RED)
+                    }else{
+                        EmailCheckText.setText("추가 성공!")
+                        EmailCheckText.setTextColor(Color.BLUE)
+                        teamMember.add(MemberEmail)
+                        updateText(teamMember)
+                    }
+
                 }else{
                     EmailCheckText.setText("이메일이 없습니다.")
                     EmailCheckText.setTextColor(Color.RED)
@@ -86,11 +110,16 @@ class MakeTeamActivity : AppCompatActivity() {
         })
     }
 
-    private fun addText(MemberEmail:String){
-        val textView = TextView(this)
-        textView.text = MemberEmail
-        MemberLayout.addView(textView)
+    private fun updateText(teamMember: HashSet<String>){
+        var member = teamMember.toArray()
+        var text = ""
+        for(i in member){
+            text+=i
+            text+=" "
+        }
+        MemberList.setText(text)
     }
+
     private fun createOkHttpClient(): OkHttpClient {
         val builder = OkHttpClient.Builder()
         val interceptor = HttpLoggingInterceptor()
@@ -98,4 +127,8 @@ class MakeTeamActivity : AppCompatActivity() {
         builder.addInterceptor(interceptor)
         return builder.build()
     }
+
+//    private fun addTeam(checkDup){
+//
+//    }
 }
