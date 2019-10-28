dfdf7f7add852f1a125ed71f66e3badf360ed06e
박홍렬 <vlhong94@gmail.com>
2019-10-28 09:16:11 +0900

    [+] Edit MainActivity.kt

--- front/app/src/main/java/com/example/findmatch/Activity/MainActivity.kt
+++ front/app/src/main/java/com/example/findmatch/Activity/MainActivity.kt
@@ -4,21 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
-import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.findmatch.R
 import com.example.findmatch.DTO.TeamDto
 import com.example.findmatch.Service.TeamService
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -28,15 +17,10 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlinx.android.synthetic.main.activity_main.frame
 import kotlinx.android.synthetic.main.activity_main.pager
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navController: NavController
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
